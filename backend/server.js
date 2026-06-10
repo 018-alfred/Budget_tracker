ed820a66bff0cc22b1c31a4b2a1b13dedec76490
018-alfred <alfredvishal25@gmail.com>
2026-06-10 19:45:37 +0530

    fixec one issue in api.js

--- backend/server.js
+++ backend/server.js
@@ -5,36 +5,23 @@ const cors = require("cors");
 
 const app = express();
 
-// CORS
-app.use(
-  cors({
-    origin: [
-      "https://budgettracker-gray.vercel.app",
-      "http://localhost:5500",
-      "http://127.0.0.1:5500"
-    ],
-    credentials: true
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
-// Health Check
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Budget Tracker API Running"
-  });
+app.get("/", (req,res)=>{
+ res.json({
+  success:true,
+  message:"Budget Tracker API Running"
+ });
 });
 
-// Routes
-app.use("/api/monthly", require("./Routes/monthlyRoutes"));
-app.use("/api/annual", require("./Routes/annualRoutes"));
-app.use("/api/year", require("./Routes/yearRoutes"));
-app.use("/api/fiveyear", require("./Routes/fiveYearRoutes"));
+app.use("/monthly", require("./Routes/monthlyRoutes"));
+app.use("/annual", require("./Routes/annualRoutes"));
+app.use("/year", require("./Routes/yearRoutes"));
+app.use("/fiveyear", require("./Routes/fiveYearRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, ()=>{
+ console.log(`Server running on ${PORT}`);
 });
