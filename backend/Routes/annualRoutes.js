082a1f362a4269282a7977d7dbb11fe44374603d
018-alfred <alfredvishal25@gmail.com>
2026-06-10 20:35:57 +0530

    fixed server.js

--- backend/Routes/annualRoutes.js
+++ backend/Routes/annualRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const clerkAuth =
-require("../middleware/clerkauth");
+require("../middleware/clerkAuth");
 
 const {
  createBudget,
