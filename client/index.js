6523c320fe6076f6fa2e050d4d6184368458cc28
018-alfred <alfredvishal25@gmail.com>
2026-06-20 17:36:38 +0530

    updated the home page

--- client/index.js
+++ client/index.js
@@ -242,3 +242,60 @@ video.addEventListener("timeupdate", () => {
     }
 
 });
+
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slideNumber = document.getElementById("slideNumber");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+    slideNumber.textContent =
+    `${index + 1} / ${slides.length}`;
+}
+
+nextBtn.addEventListener("click", () => {
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+});
+
+prevBtn.addEventListener("click", () => {
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+
+});
+
+/* Optional Auto Slide */
+
+setInterval(() => {
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+}, 6000);
